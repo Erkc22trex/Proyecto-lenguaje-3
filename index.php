85091c90d4cf9461f073d3b0e2ff28c035d6d1b4
user <user@DESKTOP-9V65GE2>
2023-10-13 18:54:01 -0600

    Prueba repositorio

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!--
+npm i bootstrap@5.3.2
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        ?>
+    </body>
+</html>
